1223942e09efdef02f674863825e8da8b4d56ce3
yuuta66jp <yuuta66jp@yahoo.co.jp>
2020-01-17 11:56:20 +0900

    [start]userpage,indexpage作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,18 @@
 class BooksController < ApplicationController
-  def index
-  end
+
+  before_action :authenticate_user!
 
   def create
+      @book = Book.new(book_params)
+      @book.user_id = current_user.user_id
+      @book.save
+      redirect_to books_path
   end
 
   def show
+  end
+
+  def index
   end
 
   def edit
@@ -16,4 +23,11 @@ class BooksController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def book_params
+      params.require(:book).permit(:title, :body)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
+
+  before_action :authenticate_user!, except: [:top, :about]
+
   def top
   end
 
   def index
+      @users = User.all
+      @book = new
   end
 
   def about
