0d4354c590fb88ab3074ddb70d2a976bcaa5997f
yuuta66jp <yuuta66jp@yahoo.co.jp>
2020-01-19 15:20:38 +0900

    [start]レイアウト編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
       @book = Book.new(book_params)
       @book.user_id = current_user.id
      if @book.save
+      flash[:notice] = "You have creatad book successfully."
       redirect_to book_path(@book.id)
      else
       @books = Book.all
@@ -33,6 +34,7 @@ class BooksController < ApplicationController
   def update
       @book = Book.find(params[:id])
       if @book.update(book_params)
+        flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book.id)
     else
       render :edit
