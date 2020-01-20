873deaeea4a5cf9079bc8b206500f024157fb33f
yuuta66jp <yuuta66jp@yahoo.co.jp>
2020-01-20 11:44:03 +0900

    [start]user制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :correct_user,only:[:edit, :update]
 
   def show
       @user = User.find(params[:id])
@@ -31,6 +32,13 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :profile_image, :introduction)
+  end
+
+  def correct_user
+    user = User.find(params[:id])
+    if current_user != user
+      redirect_to user_path(current_user.id)
+    end
   end
 
 end
