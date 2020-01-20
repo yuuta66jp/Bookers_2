eefec056ba7546cce588310ade28573147723f52
yuuta66jp <yuuta66jp@yahoo.co.jp>
2020-01-20 11:58:43 +0900

    [finish]user制限

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update]
 
   def create
       @book = Book.new(book_params)
@@ -52,6 +53,13 @@ class BooksController < ApplicationController
 
   def book_params
       params.require(:book).permit(:title, :body)
+  end
+
+  def correct_user
+      book = Book.find(params[:id])
+      if current_user.id != book.user.id
+        redirect_to books_path
+      end
   end
 
 end
