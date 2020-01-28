645bddd7cf5bc98df08829512abbfddc83de470e
yuuta66jp <yuuta66jp@yahoo.co.jp>
2020-01-28 16:30:18 +0900

    [start]検索機能

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,25 +5,15 @@ class RelationshipsController < ApplicationController
 	def create
 		user = User.find(params[:follow_id])
 		following = current_user.follow(user)
-		if following.save
-			flash[:success] = 'ユーザーをフォローしました'
-			redirect_to user
-		else
-			flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-			redirect_to user
-		end
+		following.save
+	    redirect_to user
 	end
 
 	def destroy
 		user = User.find(params[:follow_id])
 		following = current_user.unfollow(user)
-		if following.destroy
-			flash[:success] = 'ユーザーのフォローを解除しました'
-			redirect_to user
-		else
-			flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
-			redirect_to user
-		end
+		following.destroy
+	    redirect_to user
 	end
 
 	private
